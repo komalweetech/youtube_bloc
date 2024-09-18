41f8bc18d6f453bd5e7f313540320e65191c6485
komal kathiriya <komalweetech211@gmail.com>
2024-09-18 17:27:21 +0530

    first commit

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Youtube',style: TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+}
